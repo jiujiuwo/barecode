f1eab78285b0c511014272f7d3cd9abce6310455
jiujiuwo <lihaifeng94@qq.com>
2021-06-06 10:09:55 +0800

    条形码生成 1.0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,112 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="barecodes">
+    <el-row>
+      <el-col :span="6"
+        ><div class="grid-content bg-purple">
+          <el-link type="primary" :underline="false" href="#"
+            >鼠标点击框外自动生成条形码
+          </el-link>
+        </div></el-col
+      >
+      <el-col :span="12"
+        ><div class="grid-content bg-purple-light">
+          <el-input
+            type="textarea"
+            :autosize="{ minRows: 4, maxRows: 8 }"
+            placeholder="将运单号复制到这里"
+            v-model="text"
+            @blur="textAreaOnBlur"
+            resize="false"
+            clearable
+          >
+          </el-input>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+        共<el-link type="primary" :underline="false" href="#">{{
+          listSize
+        }}</el-link
+        >条数据</el-col
+      >
+    </el-row>
+    <el-divider></el-divider>
+    <el-row>
+      <el-col :span="2"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="20"
+        ><el-sapce wrap><div id="codes"></div></el-sapce>
+      </el-col>
+      <el-col :span="2"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import jsbarcode from "jsbarcode";
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  props: {},
+  data() {
+    return { listSize: 0, strList: {}, text: "" };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    textAreaOnBlur() {
+      this.strList = this.text.split(/[\s\n]/);
+      console.info(this.strList);
+      var codeRoot = document.getElementById("codes");
+      codeRoot.innerHTML = "";
+      this.listSize = 0;
+      try {
+        for (var i = 0; i < this.strList.length; i++) {
+          if (this.strList[i].length > 2) {
+            codeRoot.innerHTML =
+              codeRoot.innerHTML + '<svg id="' + this.strList[i] + '"></svg>';
+            jsbarcode("#" + this.strList[i], this.strList[i]);
+            this.listSize += 1;
+          }
+        }
+      } catch (e) {
+        this.$message.error("输入内容有误");
+      } finally {
+        console.info();
+      }
+    },
   },
 };
 </script>
@@ -126,5 +92,17 @@ li {
 }
 a {
   color: #42b983;
+}
+
+#codes {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  svg {
+    margin: 10px;
+  }
+}
+.el-link {
+  font-size: 20px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>条形码生成器</h1>
+  <div class="条形码生成">
+    <HelloWorld />
   </div>
 </template>
 
